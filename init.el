f14c235357a59878b6fe721ee7d09ee9eef4d338
yellowtaoworkshop <gavinhuangtao@gmail.com>
2018-11-18 13:12:10 +0800

    enable spell check

--- init.el
+++ init.el
@@ -47,7 +47,7 @@ values."
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
      spell-checking
-     ;; syntax-checking
+     syntax-checking
      auto-completion
      gavin
      ;; version-control
