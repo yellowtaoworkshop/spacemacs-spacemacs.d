5c20740c83dafa658eef895a8fd1f964c3f6df9b
gavinhuangtao@gmail.com <gavinhuangtao@gmail.com>
2018-12-01 11:46:13 +0800

    update some ui: perl keywords and theme

--- init.el
+++ init.el
@@ -133,8 +133,8 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
-                         dracula
                          spacemacs-dark
+                         dracula
                          tsdh-light
                          leuven
                          molokai
@@ -364,4 +364,4 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-builtin-face ((t (:foreground "gold")))))
--- layers/gavin-ui/config.el
+++ layers/gavin-ui/config.el
@@ -1,3 +1,9 @@
 ;; enable the cursor blink
 (blink-cursor-mode t)
 
+;; hilight perl builfin function name
+(font-lock-add-keywords 'perl-mode '(("\\<print\\(f\\)?\\>" . font-lock-builtin-face)
+                                     ("\\<open\\>" . font-lock-builtin-face)
+                                     ("\\<close\\>" . font-lock-builtin-face)
+                                     ("\\<\\(or\\)\\>" . font-lock-keyword-face))) 
+
